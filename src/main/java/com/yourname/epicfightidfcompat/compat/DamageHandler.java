e146277c9ee8d31bfd2dfdd072d3f4b9db3af0d8
saltsnuggler <paperhawk3@gmail.com>
2024-11-14 12:40:50 +0100

    DamageHandler.java

---
+++ src/main/java/com/yourname/epicfightidfcompat/compat/DamageHandler.java
@@ -0,0 +1,8 @@
+package com.yourname.epicfightidfcompat.compat;
+
+public class DamageHandler {
+    public static float handleDamage(float baseDamage) {
+       
+        return baseDamage; 
+    }
+}
